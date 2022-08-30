dc4675b396a48a7669a0d6909828a75e9dd7cb69
lintenri <112460885+lintenri@users.noreply.github.com>
2022-08-30 12:02:27 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = "herlin.py",
     version = "0.1",
     py_modules = ['herlin'],
-    url = "https://github.com/indraxz/jewer.git",
+    url = "https://github.com/lintenri/herlin.git",
     author = "lintenri",
     install_requires = ["pyngrok"]
 )
