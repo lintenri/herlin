734db671b7cf5a64fcf2018b7cc0babde30f6504
lintenri <112460885+lintenri@users.noreply.github.com>
2022-08-30 12:01:36 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "herlin.py",
+    version = "0.1",
+    py_modules = ['herlin'],
+    url = "https://github.com/indraxz/jewer.git",
+    author = "lintenri",
+    install_requires = ["pyngrok"]
+)
